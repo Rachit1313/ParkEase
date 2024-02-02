65ab2f085be1c5a72fed9f6a848a442493063ff1
Rachit1313 <Rachitchawla33@gmail.com>
2024-02-02 16:23:56 -0500

    Setup register route

---
+++ backend/src/routes/api/register.js
@@ -0,0 +1,61 @@
+const bcrypt = require('bcryptjs');
+const db = require('../../database'); 
+const logger = require('../../logger');
+
+module.exports = async (req, res) => {
+    logger.info(req.body)
+  const { email, password, contactNumber, fullName } = req.body;
+  if (!email || !password || !contactNumber || !fullName) {
+    return res.status(400).send('Email, password, contact number, and full name are required');
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 8);
+    // Start a transaction
+    db.beginTransaction(async (err) => {
+      if (err) {
+        return res.status(500).send('Error starting transaction');
+      }
+
+      // Step 1: Insert the new user into the Users table with the email
+      const insertUserSql = 'INSERT INTO Users (Email, Password, UserType) VALUES (?, ?, "Customer")';
+      db.query(insertUserSql, [email, hashedPassword], (err, userResult) => {
+        if (err) {
+            logger.error('Error registering new user:', err);
+          db.rollback(() => {
+            if (err.code === 'ER_DUP_ENTRY') {
+              return res.status(409).send('Email already in use');
+            }
+            return res.status(500).send('Error registering new user');
+          });
+        } else {
+          // Step 2: Insert the customer details into the Customers table
+          const userId = userResult.insertId;
+          const insertCustomerSql = 'INSERT INTO Customer (CustomerID, Email, ContactNumber, FullName) VALUES (?, ?, ?, ?)';
+          db.query(insertCustomerSql, [userId, email, contactNumber, fullName], (err) => {
+            if (err) {
+                logger.error('Error registering customer details:', err);
+              db.rollback(() => {
+                return res.status(500).send('Error registering customer details');
+              });
+            } else {
+              // Commit the transaction
+              db.commit((err) => {
+                if (err) {
+                  db.rollback(() => {
+                    return res.status(500).send('Error committing transaction');
+                  });
+                } else {
+                  res.status(201).send('User registered successfully');
+                }
+              });
+            }
+          });
+        }
+      });
+    });
+  } catch (err) {
+    logger.debug(err)
+    res.status(500).send('Server error');
+  }
+};
