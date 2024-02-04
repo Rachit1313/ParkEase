00b7cbad8d3ca60d677c5fc99966c83ec648f488
Heavendeep Kaur Munjal <hmunjal3@myseneca.ca>
2024-02-04 16:38:34 -0500

    added unit test for forgot password

---
+++ backend/tests/unit/forgotPassword.test.js
@@ -0,0 +1,84 @@
+
+// File: login.test.js
+// ------------------
+// This file contains unit test for the login 
+
+// Author: Heavendeep kaur
+// Date: February 3, 2024
+
+// Import the necessary modules
+const forgotPassword = require('../../src/routes/api/forgotPassword');
+const db = require('../../src/database');
+const nodemailer = require('nodemailer');
+afterAll(() => {
+  db.end(); // Close the database connection after all tests have completed
+});
+
+// Import dotenv and configure it
+require('dotenv').config();
+
+// Define a separate test transporter
+const testTransporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.TEST_EMAIL_USER,
+    pass: process.env.TEST_EMAIL_PASSWORD
+  }
+});
+
+// Define a test user email
+const testUserEmail = 'test@example.com';
+
+// Mock request and response objects
+const req = { body: { email: testUserEmail } };
+let res;
+
+// Unit test
+describe('forgotPassword route', () => {
+  beforeEach(() => {
+    // Reset response object before each test
+    res = {
+      status: jest.fn(() => res),
+      send: jest.fn()
+    };
+  });
+
+  it('should return 400 if email is not provided', async () => {
+    const reqWithoutEmail = { body: {} };
+    await forgotPassword(reqWithoutEmail, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith('Email is required');
+  });
+
+  it('should return 500 if an error occurs during database query', async () => {
+    const originalQueryFn = db.query;
+    db.query = jest.fn((sql, values, callback) => {
+      callback(new Error('Database error'), null);
+    });
+
+    await forgotPassword(req, res);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith('Error updating password');
+
+    // Restore original function after the test
+    db.query = originalQueryFn;
+  });
+
+  it('should return 404 if no user found with the provided email', async () => {
+    const originalQueryFn = db.query;
+    db.query = jest.fn((sql, values, callback) => {
+      callback(null, { affectedRows: 0 });
+    });
+
+    await forgotPassword(req, res);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.send).toHaveBeenCalledWith('No user found with the provided email');
+
+    // Restore original function after the test
+    db.query = originalQueryFn;
+  });
+
+  
+});
