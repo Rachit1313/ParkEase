473e98244bc4045b6b00acd059ffe08cc1f28541
Rachit1313 <Rachitchawla33@gmail.com>
2024-02-04 18:31:29 -0500

    Revert "Replaced db connection with db pool to avoid timeout"
    This reverts commit 754202a192197593f3c4e18611c184fa5c16329b.

--- backend/src/database.js
+++ backend/src/database.js
@@ -9,20 +9,19 @@ require('dotenv').config();
 const mysql = require('mysql');
 const logger = require('./logger');
 
-// MySQL connection pool
-const db = mysql.createPool({
+// MySQL connection
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
 });
 
-db.getConnection((err, connection) => {
+db.connect((err) => {
   if (err) {
     logger.error('Error connecting to MySQL', err);
     throw err;
   }
-  if (connection) connection.release();
   logger.info('Connected to MySQL');
 });
 
