b2913635c06fa5af755fbedf1a9c3517a5dea698
Ayush1110 <122387777+Ayush1110-11@users.noreply.github.com>
2024-02-12 12:49:16 -0500

    Update ProtectedRoutes.js

--- frontend/src/routes/ProtectedRoutes.js
+++ frontend/src/routes/ProtectedRoutes.js
@@ -1,3 +1,7 @@
+/* Author: Ayush Shah
+Subject: PRJ 666ZAA
+Professor Name: Clint Macdonald */
+
 // ProtectedRoutes.js
 import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
