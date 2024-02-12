b6f943427555828c6c42bc56f896e99d488c7c4d
malhotraraghav2003 <malhotraraghav2003@gmail.com>
2024-02-11 22:39:31 -0500

    Update Frontend

---
+++ frontend/src/auth/useAuthCheck.js
@@ -0,0 +1,24 @@
+// useAuthCheck.js
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
+const useAuthCheck = () => {
+    const navigate = useNavigate();
+
+    const isLoggedIn = Cookies.get('token') !== undefined;
+    const userType = localStorage.getItem('userType');
+  
+    const handleAuth = () => {
+      if (!isLoggedIn) {
+        navigate('/signin');
+      } else if (userType === 'Customer' && window.location.pathname.startsWith('/admin')) {
+        navigate('/home');
+      } else if (userType === 'Admin' && window.location.pathname.startsWith('/home')) {
+        navigate('/admin');
+      }
+    };
+  
+    return handleAuth;
+};
+
+export default useAuthCheck;
