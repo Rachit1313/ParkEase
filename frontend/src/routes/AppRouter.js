78304e59db9fc342f342f078c40c7cb4597e1b32
Ayush1110 <122387777+Ayush1110-11@users.noreply.github.com>
2024-02-22 10:20:10 -0500

    Update AppRouter.js

--- frontend/src/routes/AppRouter.js
+++ frontend/src/routes/AppRouter.js
@@ -1,12 +1,14 @@
 /* Author: Ayush Shah
 Subject: PRJ 666ZAA
-Professor Name: Clint Macdonald */
+Professor Name: Clint MacDonald */
 
 import SignUp from '../components/signup';
 import SignIn from '../components/signin';
 import ForgotPassword from '../components/forgot_password';
 import AdminPanelHome from '../components/admin_home';
 import CustomerPanelHome from '../components/customer_home';
+import BookingPage from '../components/booking';
+import PaymentPage from '../components/payment';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProtectedRoutes from './ProtectedRoutes';
@@ -15,16 +17,25 @@ const AppRouter = () => {
     return (
       <Router>
         <Routes>
-          <Route path="/signin" element={<SignIn/>} />
+         
           <Route path="/forgot-password" element={<ForgotPassword/>} />
-          <Route path="/" element={<SignUp/>} />
+          
+          <Route path="/signup" element={<SignUp/>} />
+          
+          
           <Route element={<ProtectedRoutes />}>
-            <Route path="/admin" element={<AdminPanelHome />} />
+            <Route path="/" element={<SignIn/>} />
+            <Route path="/book" element={<BookingPage />} />
+            <Route path="/payment" element={<PaymentPage />} />
             <Route path="/home" element={<CustomerPanelHome />} />
+            <Route path="/admin" element={<AdminPanelHome />} />
+           
          </Route>
         </Routes>
       </Router>
     );
   };
+  
+  export default AppRouter;
   
   export default AppRouter;
