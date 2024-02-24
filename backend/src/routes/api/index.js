6f3473baed7c8a5b63f34f9201c60b2980c7f3c3
Heavendeep Kaur Munjal <hmunjal3@myseneca.ca>
2024-02-24 14:45:52 -0500

    added api route in index.js

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -21,6 +21,8 @@ router.get('/booking-history/:customerId', require('./getBookingHistory'));
 router.get('/garageList', require('./getGarages') );
 router.post('/create-booking', require('./createBooking'));
 router.post('/process-payment', require('./processPayment'));
+router.post('/add-vehicles', require('./addCustomerVehicles'));
+
 // Other routes will go here later on...
 
 module.exports = router;
