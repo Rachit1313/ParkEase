03da3044985670bfc637e7e42ff6ab5e0cc87ccf
raghav malhotra <102170209+malhotraraghav2003@users.noreply.github.com>
2024-03-04 10:05:03 -0500

    Update signup.jsx

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -1,3 +1,7 @@
+/* Author: Raghav Malhotra
+Subject: PRJ 666ZAA
+Professor Name: Clint Macdonald */
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
