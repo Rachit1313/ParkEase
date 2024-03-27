2d3ce84a51460adc9bbf2d67d79c3124e9535757
Heavendeep Kaur Munjal <hmunjal3@myseneca.ca>
2024-03-26 20:14:14 -0400

    added

--- backend/src/routes/ticket/updateTicketStatus.js
+++ backend/src/routes/ticket/updateTicketStatus.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
   const { ticketID, status } = req.body;
   const sql = `UPDATE Tickets SET Status = ? WHERE TicketID = ?`;
 
-  db.query(sql, [status, ticketID], (err, result) => {
+  db.query(sql, [status, ticketID], (err) => {
     if (err) {
       console.error('Error updating ticket status:', err);
       return res.status(500).send({ message: 'Error updating ticket status' });
