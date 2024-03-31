654c52cc1cf5b3a0922a7e2939069a7b270fb310
Rachit1313 <Rachitchawla33@gmail.com>
2024-03-31 09:07:43 -0400

    Updated response ticket based on est timezone

--- backend/src/routes/ticket/respondToTicket.js
+++ backend/src/routes/ticket/respondToTicket.js
@@ -1,10 +1,11 @@
-const db = require('../../database'); // Adjust path as necessary
+const db = require('../../database');
 
 module.exports = (req, res) => {
   const { ticketID, responderID, responseText } = req.body;
-  const sql = `INSERT INTO TicketResponses (TicketID, ResponderID, ResponseText) VALUES (?, ?, ?)`;
+  const currentTime = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
+  const sql = `INSERT INTO TicketResponses (TicketID, ResponderID, ResponseText, CreatedTime) VALUES (?, ?, ?, ?)`;
 
-  db.query(sql, [ticketID, responderID, responseText], (err, result) => {
+  db.query(sql, [ticketID, responderID, responseText, currentTime], (err, result) => {
     if (err) {
       console.error('Error responding to ticket:', err);
       return res.status(500).send({ message: 'Error responding to ticket' });
